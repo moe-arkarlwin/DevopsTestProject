2e4d447381abc09c07e55227019dade16ed75ca6
Dr. Moe Ar Kar Lwin <moearkarlwin@imu.edu.mm>
2026-08-25 13:48:56 +0630

    First Commit

---
+++ src/main/java/com/test/Main.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class Main {
+
+    static void main() {
+        IO.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
